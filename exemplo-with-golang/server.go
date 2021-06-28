c5cdd42532bb1061dd1688e417423e7d85e418a3
jamilson <jamilsonmelow@gmail.com>
2021-06-27 21:41:41 -0300

    create configmap with envfile

--- exemplo-with-golang/server.go
+++ exemplo-with-golang/server.go
@@ -1,13 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/configmap", configMapHandler)
+
 	http.ListenAndServe(":8080", nil)
+
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("<h1>Hello World!</h1>"))
+}
+
+func configMapHandler(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile(".envfile")
+
+	if err != nil {
+		log.Fatalf("Error to reading file: ", err)
+	}
+
+	fmt.Fprintf(w, "env file: %s.", string(data))
 }
